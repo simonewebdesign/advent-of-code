80d655618713dc77f5d54b4e802e43484397480a
Simone <hello@simonewebdesign.it>
2019-12-01 20:24:22 +0000

    Cleanup

--- 2019/1-tyranny-of-rocket-equation/src/app/core.clj
+++ 2019/1-tyranny-of-rocket-equation/src/app/core.clj
@@ -1,12 +1,9 @@
 (ns app.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 (defn calc-fuel
-  "Fuel required to launch a given module is based on its mass. Specifically, to finalc the fuel required for a module, take its mass, divide by three, round down, and subtract 2."
+  "Fuel required to launch a given module is based on its mass.
+   Specifically, to finalc the fuel required for a module, take its mass,
+   divide by three, round down, and subtract 2."
   [mass]
   (- (int (Math/floor (/ mass 3))) 2))
 
@@ -18,11 +15,6 @@
     (clojure.string/split-lines
       (slurp "input.txt"))))
 
-(defn sum-fuels
-  "Sums all the values in input.txt"
-  [fuels]
-  (reduce + fuels))
-
 (defn get-calc-and-sum
   []
-  (sum-fuels (map calc-fuel (get-fuels))))
+  (reduce + (map calc-fuel (get-fuels))))
--- 2019/1-tyranny-of-rocket-equation/test/app/core_test.clj
+++ 2019/1-tyranny-of-rocket-equation/test/app/core_test.clj
@@ -2,27 +2,24 @@
   (:require [clojure.test :refer :all]
             [app.core :refer :all]))
 
-; (deftest a-test
-;   (testing "FIXME, I fail."
-;     (is (= 1 1))))
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
 
-; (deftest find-fuel1
-;   (is (= (find-fuel 12) 2)))
+(deftest calc-fuel1
+  (is (= (calc-fuel 12) 2)))
 
-; (deftest find-fuel2
-;   (is (= (find-fuel 14) 2)))
+(deftest calc-fuel2
+  (is (= (calc-fuel 14) 2)))
 
-; (deftest find-fuel3
-;   (is (= (find-fuel 1969) 654)))
+(deftest calc-fuel3
+  (is (= (calc-fuel 1969) 654)))
 
-; (deftest find-fuel4
-;   (is (= (find-fuel 100756) 33583)))
+(deftest calc-fuel4
+  (is (= (calc-fuel 100756) 33583)))
 
-; (deftest test-get-fuels
-;   (is (= (get-fuels) [123 456])))
-
-; (deftest test-sum-fuels
-;   (is (= (sum-fuels) (+ 123 456))))
+(deftest test-get-fuels
+  (is (= (get-fuels) [12 14 1969 100756])))
 
 (deftest test-get-calc-and-sum
   (is (= (get-calc-and-sum) (+ 33583 654 2 2))))
