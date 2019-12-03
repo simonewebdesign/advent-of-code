e09b1cb39d1b6d797387fded79812e1fbc6d2a87
Simone <hello@simonewebdesign.it>
2019-12-03 13:13:02 +0000

    Cleanup

--- 2019/1-tyranny-of-rocket-equation/src/app/core.clj
+++ 2019/1-tyranny-of-rocket-equation/src/app/core.clj
@@ -15,35 +15,6 @@
     (let [fuel (calc-fuel res)]
       (if (> fuel 0)
         (recur (+ acc fuel) fuel) acc))))
-  ; (if (> (calc-fuel mass) 0))
-  ;   (reduce + (calc-fuel)))
-
-
-  ; (if (> (calc-fuel mass) 0)
-  ;   (+ (accumulate-fuel mass))
-  ;   0))
-
-  ; (reduce
-  ;   (fn [acc i]
-  ;     (+ acc (calc-fuel mass)))
-  ;   0
-  ;   (if (> mass 0)
-  ;     (accumulate-fuel mass)
-  ;     0)))
-
-  ; (if (<= (calc-fuel mass) 0)
-  ;   0
-  ;   (accumulate-fuel mass)))
-
-  ; (reduce
-  ;   (fn [acc i]
-  ;     (if > i 0
-  ;       (accumulate=))
-  ;   ))
-
-  ; (if (<= mass 0)
-  ;   mass
-  ;   (calc-fuel)))
 
 (defn get-fuels
   "Parse input.txt"
