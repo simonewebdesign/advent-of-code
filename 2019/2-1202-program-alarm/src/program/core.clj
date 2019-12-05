8e6d1e5517a7885884f0f324f490e4aba26609e3
Simone <hello@simonewebdesign.it>
2019-12-05 13:35:21 +0000

    Cracked

--- 2019/2-1202-program-alarm/src/program/core.clj
+++ 2019/2-1202-program-alarm/src/program/core.clj
@@ -39,75 +39,34 @@
   (map to-int list))
 
 
-; CONTINUE FROM HERE
-; (defn process
-;   [seq idx]
-;   (let [
-;     op (first seq)
-;     ]
-;     (case op
-;       "1" (let [
-;         in1-pos (nth seq (+ 1 idx))
-;         in2-pos (nth seq (+ 2 idx))
-;         out-pos (nth seq (+ 3 idx))
-;         in1-val (nth seq in1-pos)
-;         in2-val (nth seq in2-pos)
-;         out-new-val (+ in1-val in2-val)
-;         new-seq (assoc seq out-pos out-new-val)
-;       ]
-;       (process new-seq (+ 4 idx)))
-;       "2" (let [
-;         in1-pos (nth seq (+ 1 idx))
-;         in2-pos (nth seq (+ 2 idx))
-;         out-pos (nth seq (+ 3 idx))
-;         in1-val (nth seq in1-pos)
-;         in2-val (nth seq in2-pos)
-;         out-new-val (* in1-val in2-val)
-;         new-seq (assoc seq out-pos out-new-val)
-;       ]
-;       (process new-seq (+ 4 idx)))
-;       "99" seq)))
+(defn process
+  [seq idx]
+  (let [
+    op (nth seq idx)
+    ]
+    (case op
+      1 (let [
+        in1-pos (nth seq (+ 1 idx))
+        in2-pos (nth seq (+ 2 idx))
+        out-pos (nth seq (+ 3 idx))
+        in1-val (nth seq in1-pos)
+        in2-val (nth seq in2-pos)
+        out-new-val (+ in1-val in2-val)
+        new-seq (assoc seq out-pos out-new-val)
+      ]
+        (process new-seq (+ 4 idx)))
+      2 (let [
+        in1-pos (nth seq (+ 1 idx))
+        in2-pos (nth seq (+ 2 idx))
+        out-pos (nth seq (+ 3 idx))
+        in1-val (nth seq in1-pos)
+        in2-val (nth seq in2-pos)
+        out-new-val (* in1-val in2-val)
+        new-seq (assoc seq out-pos out-new-val)
+      ]
+        (process new-seq (+ 4 idx)))
+      99 seq
+      seq)))
 
 ; (println "OUTPUT:")
 ; (println (process list 0))
-
-      ; "1" (let [
-      ;     new-val (+ (nth seq (+ 1 idx)) (nth seq (+ 2 idx)))
-      ;     new-idx (nth seq (+ 3 idx))
-      ;     new-seq (assoc seq idx new-idx)
-      ;   ]
-      ;   (process seq (+ 4 idx)))
-      ; "2" (let [
-      ;     new-val (* (nth seq (+ 1 idx)) (nth seq (+ 2 idx)))
-      ;     new-idx (nth seq (+ 3 idx))
-      ;     new-seq (assoc seq idx new-idx)
-      ;   ]
-      ;   (process seq (+ 4 idx)))
-      ;   )))
-
-; (defn op->result
-;   [val op]
-;   (case op
-;     :sum val +))
-
-; useful functions:
-; nth seq idx
-; update
-
-; 0 1 2 3
-
-; 1 5 6 0
-
-; case
-;   sum:
-;     newVal = seq[currIdx+1] + seq[currIdx+2]
-;     newIdx = seq[currIdx+3]
-;     seq[newIdx] = newVal
-
-;     process(seq, currIdx+4)
-
-;   mult:
-;     ...
-
-;   halt:
-;     return seq
--- 2019/2-1202-program-alarm/test/program/core_test.clj
+++ 2019/2-1202-program-alarm/test/program/core_test.clj
@@ -12,7 +12,6 @@
   (is (= (to-int "456") 456)))
   ;(is (= (to-int "123\n") 123))) ; java.lang.NumberFormatException
 
-
 ; (deftest test-read-string
 ;   (is (= (read-string "123") 123))
 ;   (is (= (read-string "123") 123))
@@ -38,15 +37,26 @@
   (is (= (numbers ["1" "2" "3"]) [1 2 3])))
 
 
-; (deftest foo
-;   (is (= (input "test.txt") [1 2 3])))
+(deftest test-process-simple-sum
+  (testing "returns the altered output"
+    (is (= (process [1 0 0 0 99] 0) [2 0 0 0 99]))))
 
-; (deftest test-sum
-;   (is (= (sum 1 2) 3)))
 
-; (deftest test-mult
-;   (is (= (mult 1 2) 2)))
+(deftest test-process-simple-mult
+  (testing "returns the altered output"
+    (is (= (process [2 3 0 3 99] 0) [2 3 0 6 99]))))
 
-; (deftest test-halt
-;   (testing "returns the altered output"
-;     (is (= (halt "1,9,10,3,2,3,11,0,99,30,40,50") "3500,9,10,70,2,3,11,0,99,30,40,50"))))
+
+(deftest test-process
+  (testing "returns the altered output"
+    (is (= (process [1 9 10 3 2 3 11 0 99 30 40 50] 0) [3500 9 10 70 2 3 11 0 99 30 40 50]))))
+
+
+(deftest test-process2
+  (testing "returns the altered output"
+    (is (= (process [2 4 4 5 99 0] 0) [2 4 4 5 99 9801]))))
+
+
+(deftest test-process3
+  (testing "returns the altered output"
+    (is (= (process [1 1 1 4 99 5 6 0 99] 0) [30 1 1 4 2 5 6 0 99]))))
